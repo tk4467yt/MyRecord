4f72a463d87fb31e9d0b034dcb8ebd10ccbb2f7a
qin <brokenmoon@126.com>
2017-05-08 22:28:06 +0800

    tc

---
+++ MyRecordProj/MyRecordProj/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  MyRecordProj
+//
+//  Created by  qin on 2017/5/8.
+//  Copyright © 2017年  qin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
---
+++ MyRecordProj/MyRecordProj/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  MyRecordProj
+//
+//  Created by  qin on 2017/5/8.
+//  Copyright © 2017年  qin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
