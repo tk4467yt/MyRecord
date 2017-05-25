a92cfcb1e843636db4f6031e10dbfc6bc6b5f34e
qin <brokenmoon@126.com>
2017-05-25 22:46:50 +0800

    tc

---
+++ MyRecordProj/MyRecordProj/PhotoSelection/PhotoSelectionContainerNavVC.h
@@ -0,0 +1,13 @@
+//
+//  PhotoSelectionContainerNavVC.h
+//  MyRecordProj
+//
+//  Created by  qin on 2017/5/25.
+//  Copyright © 2017年  qin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PhotoSelectionContainerNavVC : UINavigationController
+
+@end
---
+++ MyRecordProj/MyRecordProj/PhotoSelection/PhotoSelectionRootViewController.h
@@ -0,0 +1,13 @@
+//
+//  PhotoSelectionRootViewController.h
+//  MyRecordProj
+//
+//  Created by  qin on 2017/5/25.
+//  Copyright © 2017年  qin. All rights reserved.
+//
+
+#import "MyRootViewController.h"
+
+@interface PhotoSelectionRootViewController : MyRootViewController
+
+@end
