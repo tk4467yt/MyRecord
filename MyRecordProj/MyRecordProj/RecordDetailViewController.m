6c9f60887295297f3c089d56f457301a565d772d
qin <brokenmoon@126.com>
2017-06-07 22:46:07 +0800

    tc

--- MyRecordProj/MyRecordProj/RecordDetailViewController.m
+++ MyRecordProj/MyRecordProj/RecordDetailViewController.m
@@ -14,6 +14,7 @@
 #import "MyCommonHeaders.h"
 #import "RecordSection.h"
 #import "RecordSectionItem.h"
+#import "CreateRecordViewController.h"
 
 @interface RecordDetailViewController () <UITableViewDelegate,UITableViewDataSource>
 @property (weak, nonatomic) IBOutlet UITableView *tbRecordDetail;
@@ -28,12 +29,25 @@
     // Do any additional setup after loading the view.
     
     self.navigationItem.title=NSLocalizedString(@"Detail", @"");
+    self.navigationItem.rightBarButtonItem=[[UIBarButtonItem alloc] initWithTitle:NSLocalizedString(@"Edit", @"") style:UIBarButtonItemStylePlain target:self action:@selector(action2edit)];
     
     [self.tbRecordDetail registerNib:[UINib nibWithNibName:@"RecordDetailTitleTableViewCell" bundle:[NSBundle mainBundle]] forCellReuseIdentifier:[CellIdInfo cellIdForRecordDetailTitle]];
     [self.tbRecordDetail registerNib:[UINib nibWithNibName:@"RecordDetailTxtTableViewCell" bundle:[NSBundle mainBundle]] forCellReuseIdentifier:[CellIdInfo cellIdForRecordDetailTxt]];
     [self.tbRecordDetail registerNib:[UINib nibWithNibName:@"RecordDetailImageTableViewCell" bundle:[NSBundle mainBundle]] forCellReuseIdentifier:[CellIdInfo cellIdForRecordDetailImage]];
     
+    [[MyCustomNotificationObserver sharedObserver] addCustomOvserverWithDelegate:self andKey:CUSTOM_NOTIFICATION_FOR_DB_RECORD_INFO_UPDATE];
+    
     [self updateRecordSections];
+}
+
+- (void)action2edit
+{
+    UIViewController *initVC=[MyUtility getInitViewControllerFromSB:@"CreateRecord" withBundle:nil];
+    if (nil != initVC) {
+        CreateRecordViewController *recordVC=(CreateRecordViewController *)initVC;
+        recordVC.editingRecordInfo=[DbHandler getRecordInfoWithRecordId:self.recordInfo.recordId];
+        [MyUtility pushViewControllerFromNav:self.navigationController withTargetVC:recordVC animated:YES];
+    }
 }
 
 - (void)updateRecordSections
@@ -68,11 +82,29 @@
             }
         }
     }
+    
+    [self.tbRecordDetail reloadData];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark MyCustomNotificationActionDelegate
+-(void)didReceivecMyCustomNotification:(NSDictionary *)notificationDict
+{
+    if (nil == notificationDict) {
+        return;
+    }
+    NSString *key2check=notificationDict[MyCustomNotificationContent_key];
+    if ([key2check isEqualToString:CUSTOM_NOTIFICATION_FOR_DB_RECORD_INFO_UPDATE]) {
+        self.recordInfo=[DbHandler getRecordInfoWithRecordId:self.recordInfo.recordId];
+        
+        [self updateRecordSections];
+    } else {
+        [super didReceivecMyCustomNotification:notificationDict];
+    }
 }
 
 #pragma mark override
